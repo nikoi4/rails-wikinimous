77bf2ed10f809cdb2684f4dc79d175676ae81b92
Nicolas Caif <r.nicolascaif@gmail.com>
2019-02-13 22:28:27 -0300

    seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+9.times { Article.create(title: Faker::Hacker.adjective, content: Faker::Hacker.say_something_smart) }
